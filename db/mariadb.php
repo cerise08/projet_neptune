993fcac9fc39a3fe1a2eeb5dece697c02d064f5f
Cerise <c.druaertdupriez@ecoles-epsi.net>
2025-12-10 17:25:10 +0100

    Delete db/mariadb.php

--- db/mariadb.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=projet-neptune', 'login4441', 'WKNOONYSJfxbTQS');
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-?>
