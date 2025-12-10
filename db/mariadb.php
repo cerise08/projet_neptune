dee33e5cd7ea520fbc26093f04c4aecdfbda0232
samuel <samuel.pennequin@ecoles-epsi.net>
2025-12-10 16:28:06 +0100

    accueil

--- db/mariadb.php
+++ db/mariadb.php
@@ -2,7 +2,7 @@
 // essaye de faire le code qui est dans le bloc try
 try {
     // se connecte à la base de données et stocke la connection dans $dbh
-    $dbh = new PDO('mysql:host=localhost;dbname=projet-boite', 'login4441', 'WKNOONYSJfxbTQS');
+    $dbh = new PDO('mysql:host=localhost;dbname=projet_neptune', 'login4440', 'DHkvoIIqoichfZO');
 } catch (PDOException $e) {
     // comme la connexion n'a pas fonctionné, je stocke null dans $dbh
     $dbh =  null;
